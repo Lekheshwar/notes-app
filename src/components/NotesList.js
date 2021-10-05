db0d69a91c6119a15f6c8406d7d94303a3ed8254
Lekheshwar <hilekhesh20@gmail.com>
2021-10-05 15:42:51 +0530

    initial Commit

---
+++ src/components/NotesList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Note from "./Note";
+import AddNote from "./AddNote";
+
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
+  return (
+    <div className="notes-list">
+      {notes.map((note) => (
+        <Note note={note} handleDeleteNote={handleDeleteNote} />
+      ))}
+      <AddNote handleAddNote={handleAddNote} />
+    </div>
+  );
+};
+
+export default NotesList;
